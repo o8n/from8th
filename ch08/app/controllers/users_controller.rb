6645eda9fa4ca5473aabf3dca5db6b98bac0685d
okamotchan <m.okamotchan@gmail.com>
2019-03-18 01:29:43 +0900

    fin user edit, index, destroy actions

--- ch08/app/controllers/users_controller.rb
+++ ch08/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only:[:index, :edit, :update]
+  before_action :correct_user, only:[:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   # GET /users/:id
   def show
@@ -11,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new
     # => form_for @user
   end
-  
+
   # POST /users
   def create
     @user = User.new(user_params)
@@ -23,13 +29,55 @@ class UsersController < ApplicationController
       # GET "/users/#{@user.id}" => show
     else
       # Failure
-      render 'new'      
+      render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+private
   def user_params
     params.require(:user).permit(
-      :name, :email, :password, 
+      :name, :email, :password,
       :password_confirmation)
+  end
+
+  #beforeアクション
+  #ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  #正しいユーザーかを確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  #管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin
   end
 end
